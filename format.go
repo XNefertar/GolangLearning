d888b8d9b60ba023281ee9ea462357a37953d104
X_Nefertar <xnefertar@gmail.com>
2024-12-08 15:47:33 +0800

    Update_Sprintf

--- format.go
+++ format.go
@@ -13,10 +13,29 @@ import "fmt"
 // main 主体函数
 // 每个可执行程序所必须包含的
 func main() {
+	// 注意，不同于C/C++
+	// 该语言行尾不需要加上;
+	// 该行为由golang编译器自主完成
+
 	// Println 将字符串输出到控制台
 	// 该函数会在结尾自动添加换行符
 	fmt.Println("Golang")
 	// 也可以使用Print函数
 	// 但是需要显式的添加换行符\n
 	fmt.Print("Hello World\n")
+
+	// 格式化字符串(可以参考C/C++的sprintf 和 printf)
+	// Sprintf
+	var stockcode1 = 123
+	var enddate1 = "2020-12-31"
+	var url1 = "Code=%d&endDate=%s"
+	var target_url = fmt.Sprintf(url1, stockcode1, enddate1)
+	fmt.Println(target_url)
+
+	// Printf
+	var stockcode2 = 123
+	var enddate2 = "2020-12-31"
+	var url2 = "Code=%d&endDate=%s"
+	fmt.Printf(url2, stockcode2, enddate2)
+
 }
